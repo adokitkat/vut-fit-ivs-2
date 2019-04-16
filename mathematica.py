a8970d837fa9f1fa50a53141bc9d13f2466a8b27
Peter Koprda <petokoprda@gmail.com>
2019-04-17 01:10:09 +0200

    Added doxygen

--- mathematica.py
+++ mathematica.py
@@ -1,9 +1,22 @@
+## @file matematica.py
+# @brief Matematická knižnica
+# @author Jakub Mĺkvy 
+# @author Adam Múdry
+# @author Daniel Paul
+# @author Peter Koprda
+# @version 1.0
+# @date Máj 2019
+
 result = 0
 
+## @brief Trieda Math
+# @class Math
 class Math:
+    
 
+    ## @brief Konštruktor 
+    # @param result Výsledok operácie
     def __init__(self, arg : str = None):
-        
         global result
 
         if arg is None:
@@ -17,32 +30,44 @@ class Math:
             self.val = tmp
             result = self.val
 
-
+    ## @brief Funkcia na súčet
+    # @return Súčet dvoch čísel
     def __add__(self, other):
 
         tmp = self.val + other.val
         return Math(tmp)
 
+    ## @brief Funkcia na rozdiel
+    # @return Rozdiel dvoch čísel
     def __sub__(self, other):
 
         tmp = self.val - other.val
         return Math(tmp)
 
+    ## @brief Funkcia na súčin
+    # @return Súčin dvoch čísel
     def __mul__(self, other):
 
         tmp = self.val * other.val
         return Math(tmp)
 
+    ## @brief Funkcia na podiel
+    # @return Podiel dvoch čísel
     def __div__(self, other):
 
         tmp = self.val / other.val
         return Math(tmp)
 
+    ## @brief Funkcia na mocninu
+    # @return Umocnené číslo
     def __pow__(self, other):
 
         tmp = self.val ** other.val
         return Math(tmp) 
 
+    ## @brief Funkcia na obecnú odmocninu
+    # @pre n je prirodzené číslo
+    # @return Odmocnené číslo
     def root(self, n=None):
 
         if n is None:
@@ -62,20 +87,28 @@ class Math:
 
         return Math(tmp)
 
+    ## @brief Funkcia na dekadický logaritmus
+    # @return Dekadický logaritmus z čísla n
     def ln(self):
         n = 100000000.0
         return Math(n * ((self.val ** (1/n)) - 1))
 
+    ## @brief Funkcia na logaritmus
+    # @return Logaritmus
     def log(self, base):
         tmp = Math.ln(self.val)/Math.ln(base)
         return Math(tmp)
 
+    ## @brief Funkcia na čistenie stdin
+    # @return Vyčistený stdin
     def C(self):
         global result
         result = 0
         return Math()
 
-
+    ## @brief Funkcia na konvertovanie dátových typov
+    # @exception parameter value nie je číslo
+    # @return prekonvertované číslo na string
     def __str__(self):
 
         value = self.val
