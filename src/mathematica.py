f4a72c08fbb8b10f26c0018b4c8cabbaa38783d9
Adam Múdry <mudry.ado@gmail.com>
2019-04-24 05:17:03 +0200

    finale

--- src/mathematica.py
+++ src/mathematica.py
@@ -1,7 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+## @file matematica.py
+## @brief Math library
+## @authors Jakub Mĺkvy, Adam Múdry, Daniel Paul, Peter Koprda
+## @version 1.0
+## @date May 2019
+
 result = 0
 
+## @brief Trieda Math
+# @class Math
 class Math:
 
+    ## @brief Constructor 
     def __init__(self, arg : str = None):
         global result
 
@@ -9,7 +21,10 @@ class Math:
             self.val = result
         else:
             self.init_eval(str(arg))
-            
+    
+    ## @brief Tries to evaluate the expression
+    # @param self.val In-class result of operation
+    # @param result Global result of operation
     def init_eval(self, expr):
         global result
 
@@ -24,34 +39,51 @@ class Math:
                 print(e)
                 self.val = "Syntax Error"
 
+    ## @brief Function for sum
+    # @return sum of two numbers
     def __add__(self, other):
         tmp = self.val + other.val
         return Math(tmp)
 
+    ## @brief Function for subtraction
+    # @return subtracton of two numbers
     def __sub__(self, other):
         tmp = self.val - other.val
         return Math(tmp)
 
+    ## @brief Function for multiplying
+    # @return multiplication of two numbers
     def __mul__(self, other):
         tmp = self.val * other.val
         return Math(tmp)
 
+    ## @brief Function for true division
+    # @return division of two numbers, result may be float
     def __truediv__(self, other):
         tmp = self.val / other.val
         return Math(tmp)
     
+    ## @brief Function for floor division
+    # @return division of two numbers, result is integer
     def __floordiv__(self, other):
         tmp = self.val // other.val
         return Math(tmp)
 
+    ## @brief Function for modulo
+    # @return modulo of two numbers
     def __mod__(self, other):
         tmp = self.val % other.val
         return Math(tmp)
 
+    ## @brief Function for power
+    # @return squared number
     def __pow__(self, other):
         tmp = self.val ** other.val
         return Math(tmp) 
 
+    ## @brief Function for square root
+    # @pre n is unsigned integer
+    # @return square root of number
     def root(self, n=None):
 
         if self.val < 0:
@@ -73,12 +105,16 @@ class Math:
 
         return Math(tmp)
 
+    ## @brief Function for natural logarithm
+    # @return natural logarithm of number
     def ln(self):
         n = 100000000.0
         if self.val <= 0:
             return "Math Error"
         return Math(round(n * ((self.val ** (1/n)) - 1), 8))
 
+    ## @brief Function for logarithm
+    # @return logarithm of number
     def log(self, base):
         n = 100000000.0
         if self.val <= 0 or base <= 0:
@@ -86,11 +122,15 @@ class Math:
         tmp = (n * ((self.val ** (1/n)) - 1)) / (n * ((base ** (1/n)) - 1))
         return Math(round(tmp, 8))
 
+    ## @brief Function for clearing result screen
+    # @return empty screen
     def C(self):
         global result
         result = 0
         return Math()
 
+    ## @brief Function for factorial
+    # @return factorial of a number
     def factorial(self):
         result = 1
 
@@ -101,6 +141,9 @@ class Math:
             result *= i 
         return Math(result)
 
+    ## @brief Function for converting 
+    # @exception parameter value is not a number
+    # @return converted number
     def __str__(self):
         value = self.val
         try: # If float
