81589ece92b150eead56d7fb6616b341b3e8933f
Adam Múdry <mudry.ado@gmail.com>
2019-04-24 03:14:49 +0200

    added installer

---
+++ src/installer.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mkdir -p ../installer/usr/share/pasiemepytony 2>/dev/null
+cp  main.py ../installer/usr/share/pasiemepytony/main.py && chmod +x ../installer/usr/share/pasiemepytony/main.py
+cp  mathematica.py ../installer/usr/share/pasiemepytony/mathematica.py
+cp  gui.py ../installer/usr/share/pasiemepytony/gui.py
+mkdir -p ../installer/usr/local/bin 2>/dev/null
+ln -sf /usr/share/pasiemepytony/main.py ../installer/usr/local/bin/kalc00lacka
+mkdir ../installer/tmp 2>/dev/null
+cp requirements.txt ../installer/tmp/requirements.txt
+chmod +x ../installer/deb/postinst
+dpkg-deb --build ../installer/ ../installer/kalc00lacka.deb
